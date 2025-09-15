ac44b8435fcd04d28cca22af4a9609dc1cdfe746
sugandv21 <sdvsuganya@gmail.com>
2025-09-15 14:39:12 +0530

    Update urls.py

--- backend/trainee_tracker/urls.py
+++ backend/trainee_tracker/urls.py
@@ -24,9 +24,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair_short"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh_short"),
     path("api/", include("tracker.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
