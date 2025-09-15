fb1107867c507cae956471dc191029ca24d1bc18
sugandv21 <sdvsuganya@gmail.com>
2025-09-15 14:38:01 +0530

    Update urls.py

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path("", include(router.urls)),      
     path("me/", MeView.as_view(), name="me"),
     path("users/", UserListView.as_view(), name="users-list"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
 
