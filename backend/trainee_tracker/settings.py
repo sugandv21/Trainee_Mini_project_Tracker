1401a405348f57ed26e00a0dcd0b2d53cb8993fa
sugandv21 <sdvsuganya@gmail.com>
2025-09-15 13:29:30 +0530

    Update settings.py

--- backend/trainee_tracker/settings.py
+++ backend/trainee_tracker/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
 }
 
 CORS_ALLOWED_ORIGINS = [
+    "https://traineeminiprojecttracker.netlify.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
@@ -154,4 +155,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
